f4abbdc7e2dec4496595de0a6cb25f97e394ef24
Steven Torres <stevetorres7562@gmail.com>
2018-01-02 17:43:52 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,5 @@
-
+function(shout(string)) { 
+} 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
